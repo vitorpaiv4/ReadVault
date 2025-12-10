87556a8d57aa20cc877671386e27c94b7b1b9ff0
Vitor Paiva <138500099+vitorpaiv4@users.noreply.github.com>
2025-12-10 19:15:38 -0400

    Update config.php

--- config.php
+++ config.php
@@ -4,12 +4,8 @@ return [
 
     'database' => [
 
-        'driver' => 'mysql',
-        'host' => 'sql.infinityfree.com',  // Change to your host
-        'dbname' => 'your_database_name',   // Change to your database name
-        'user' => 'your_username',          // Change to your username
-        'password' => 'your_password',      // Change to your password
-        'charset' => 'utf8mb4'
+        'driver' => 'sqlite',
+        'database' => __DIR__ . '/database.sqlite'
 
     ]
 
